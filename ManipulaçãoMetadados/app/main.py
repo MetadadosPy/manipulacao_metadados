5bcc79d2d80dd727de5cf71fd1aebcb90339ec34
Ivys Emanoel Cordeiro de Souza Lima <112029836+IvysSouza07@users.noreply.github.com>
2025-05-18 20:46:23 -0300

    Ultima versão

--- ManipulaçãoMetadados/app/main.py
+++ ManipulaçãoMetadados/app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import transcribe
 
 app = FastAPI()
 
-app.include_router(transcribe.router)  # <-- Use .router aqui!
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou especifique seu front-end
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(transcribe.router)
